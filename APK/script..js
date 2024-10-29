d98875f599364a323ac469f90cbac05d207f2413
ericmkr <victorjoluke190701@gmail.com>
2024-10-29 16:17:38 +0100

    Same

--- APK/script..js
+++ APK/script..js
@@ -22,4 +22,30 @@ console.log("Signup Form Hidden:", signupForm.classList.contains('hidden'));
     }
   }
 
-  // Fonction de rechargement avec délai
+  // Fonction pour afficher l'animation de chargement avant la soumission du formulaire
+function reloadWithDelay(event) {
+  event.preventDefault(); // Empêche la soumission immédiate du formulaire
+
+  // Affiche l'overlay de chargement
+  const overlay = document.getElementById('loadingOverlay');
+  overlay.style.display = 'flex';
+
+  // Délai avant la soumission du formulaire (ajuste le délai selon les besoins)
+  setTimeout(() => {
+    document.getElementById('login-form').submit();
+  }, 5000); // Temps en millisecondes (2000 ms = 2 sec)
+}
+
+// Fonction pour naviguer vers la page d'inscription avec un délai
+function navigateToSignup(event) {
+  event.preventDefault(); // Empêche le rechargement immédiat
+
+  // Affiche l'overlay de chargement
+  const overlay = document.getElementById('loadingOverlay');
+  overlay.style.display = 'flex';
+
+  // Redirige vers la page d'inscription après un délai
+  setTimeout(() => {
+    window.location.href = "index_signin.html";
+  }, 5000); // 1 seconde de délai
+}
