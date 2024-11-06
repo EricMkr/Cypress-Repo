6ea3b01c73392c1f889a247a993636d390ea4cbe
ericmkr <victorjoluke190701@gmail.com>
2024-11-06 12:58:08 +0100

    again

--- e2e/Test.cy.js
+++ e2e/Test.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 describe('Login de mon application web.', () => {
   it('Je renseigne mes informations et je me connect', () => {
-     cy.visit('http://127.0.0.1:3000/APK/index_login.html')
+     cy.visit('http://localhost:3000')
   })
 })
